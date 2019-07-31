4e63bfec3c14eb170932637fc9795ec87fa8464b
Joel Dodge <joeldodge@gmail.com>
2019-07-31 02:01:25 -0700

    vimrc for python

--- system/env.zsh
+++ system/env.zsh
@@ -1,2 +1,2 @@
 export EDITOR='code'
-export GOOGLE_APPLICATION_CREDENTIALS='~/Documents/nlb/logstash/bqjsonkey.json'
+export GOOGLE_APPLICATION_CREDENTIALS='~/Documents/linkedin-data-generator/looker-se-c9883c686a9e.json'
