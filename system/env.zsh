ef8e27836bcef34c4d32ea86bd1906baf21de7da
Joel Dodge <joeldodge@gmail.com>
2020-03-12 16:49:21 -0700

    time to update

--- system/env.zsh
+++ system/env.zsh
@@ -1,2 +1,5 @@
 export EDITOR='vim'
-export GOOGLE_APPLICATION_CREDENTIALS='~/Documents/linkedin-data-generator/looker-se-c9883c686a9e.json'
+#export GOOGLE_APPLICATION_CREDENTIALS='/Users/joeldodge/Documents/linkedin-data-generator/looker-se-c9883c686a9e.json'
+export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
+export PYTHONBREAKPOINT='pudb.set_trace'
+export PYTHONWARNINGS='ignore'
